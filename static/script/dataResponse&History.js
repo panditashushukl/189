9fc711142400aba41a1774712c9ced99321b6c18
panditshukl <panditashushukl@gmail.com>
2025-04-13 12:17:39 +0530

    Major UI Upgradation

---
+++ static/script/dataResponse&History.js
@@ -0,0 +1,185 @@
+//This Page Contains data related to request, response, history
+const output = document.querySelector('.output-pane')
+let history = JSON.parse(localStorage.getItem('history')) || []
+const options = document.querySelector('#options');
+const equation = document.querySelector('#equation')
+
+//This Event Listenenr is for printing dynamic Results
+equation.addEventListener('input', function () {
+  if(options.value ==='simple'){
+    const equationText = equation.getValue('ascii-math').replace(/\^/g, '**');
+    try {
+      const result = eval(equationText);
+      equation.setAttribute('data-result', `= ${result.toFixed(3)}`);
+    } catch (error) {
+      equation.setAttribute('data-result', '');
+    }
+  }
+})
+
+//Send Data To Server
+function sendData(expression, calculationType){
+  const formData = new URLSearchParams();
+  formData.append('userInput', expression);
+  formData.append('calculationType', calculationType);
+
+  fetch('/BhaskarAcharya', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded', 
+    },
+    body: formData.toString()
+  })
+  .then(response => response.json())
+  .then(data => {
+    printData(data,expression)
+    setHistory(data.result,expression)
+    equation.setAttribute('data-result', '');
+    if (equation.hasAttribute('error-display')) {
+      equation.removeAttribute('error-display');
+    }
+    output.style.display = 'block'
+  })
+  .catch(error => {
+    printError(error)
+  });  
+  
+}
+
+function dynamicError(str) {
+  equation.setAttribute('error-display', str)
+  setTimeout(() => {
+    equation.removeAttribute('error-display')
+  }, 9000)
+}
+
+//Function to Submit the Data
+function submitData () {
+  const equationValue = equation.getValue('ascii-math')
+  if (equationValue === ''){
+    dynamicError('*Please enter a Expression')
+    return
+  }
+  const commaCount = (equationValue.match(/,/g) || []).length;
+  const equalCount = (equationValue.match(/=/g) || []).length;
+  if (options.value === 'linear1') {
+    if (equalCount!==1) {
+      dynamicError('*Please provide expressions in form ax+by+c=0')
+      return
+    }
+  }
+  else if (options.value === 'linear2') {
+    if (commaCount!==1) {
+      dynamicError('*Please provide comma between expressions.')
+      return
+    } else if (equalCount!==2) {
+      dynamicError('*Please provide expressions in form ax+by+c=0')
+      return
+    }
+    
+  }
+  else if (options.value === 'linear3') {
+    if (commaCount!==2) {
+      dynamicError('*Please provide comma between expressions.')
+      return
+    } else if (equalCount!==3) {
+      dynamicError('*Please provide expressions in form ax+by+cz+d=0')
+      return
+    }
+  }
+  output.innerHTML = '<div class="loading-screen"><div class="loading-spinner"></div></div>';
+  sendData(equation.getValue('ascii-math'),options.value)
+}
+
+//Event Listener on Document Load
+document.addEventListener('DOMContentLoaded', () => {
+  addingClassToOptions()
+
+  //Event Listener to Submit the Data
+  document.querySelector('#submit').addEventListener('click',submitData)
+  equation.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      submitData()
+    }
+  })
+
+  //Event Listener to clear the data
+  document.querySelector('#clear').addEventListener('click',()=>{
+    equation.setValue('')
+    equation.setAttribute('data-result', '');
+    output.style.display = 'none'
+  })
+});
+
+//Function that Prints data in Output Pane
+function printData(data,expression){
+  output.innerHTML = `
+    <div class="box-styling" id="expression">
+      <b>Input Expression:</b><br><br>
+      ${expression} 
+    </div>
+    <div class="box-styling" id="result">
+      <b>Evaluated Result:</b><br><br>
+      ${data.result.replace(/\n/g, '<br>')} 
+    </div>
+    <div class="box-styling" id="stepsContent">
+      <b>Step-by-step Solution:</b><br><br> 
+      ${data.steps.replace(/\n/g, '<br>')} 
+    </div>
+    <div class="box-styling" id="graphContent">
+      <b>Graph:</b><br><br>
+      <img src=data:image/png;base64,${data.graph}  alt="Graph" style="width: 100%; max-width: 600px;"> 
+    </div>
+  `
+}
+
+//If Error Occurred this function prints error
+function printError(error){
+  output.innerHTML = `
+    <div class="box-styling"> ${error} </div>
+  `
+}
+
+//This function is used to set history in the array and also sets in local storage
+function setHistory(result,expression) {
+  history.push({expression : expression, result : result, calculationType : options.value})
+  localStorage.setItem("history", JSON.stringify(history))
+}
+
+//This function clears the History from the local storage
+function clearHistory() {
+  localStorage.removeItem("history");
+  history = []
+  document.querySelector('#history-list').innerHTML = ""
+}
+
+//This function is used to display History in slider span
+function displayHistory(){
+  history.map(({expression,result},index)=>{
+    const historyItem =`
+        <div class="box-styling">
+          <b>Expression : </b>
+          ${expression} <br><br>
+          <b>Result:</b>
+          <pre> ${result} </pre>
+          <button class="useHistory button-style" data-index="${index}"> Use this Calculation </button>
+        </div>
+    `
+    document.querySelector('#history-list').innerHTML += historyItem
+  })
+
+  //Event Listener to use the data of history
+  document.querySelectorAll('.useHistory').forEach(button => {
+    button.addEventListener('click', (e) => {
+      const index = e.target.getAttribute('data-index'); 
+      expression = history[index].expression
+      calculationType = history[index].calculationType
+      equation.setValue(expression)
+      options.value = calculationType
+      addingClassToOptions()
+      sendData(expression,calculationType)
+      toggleSlider('null')
+    });
+  });
+}
---
+++ static/script/slider&Sidebar.js
@@ -0,0 +1,104 @@
+//This Page Contains Data about slider function changing background on sidebar option and Scroll the page 50 px below screen and toggleing Background
+document.addEventListener('DOMContentLoaded', () => {
+  // Button to change the Theme
+  const toggleButton = document.getElementById("toggleTheme");
+  toggleButton.addEventListener('click', toggleTheme)
+})
+//Function to toggleTheme
+
+const toggleTheme = () => {
+  const imglst = ['steps','graph','ido']
+  const images = document.querySelectorAll('.toggle-img');
+  const rootStyles = getComputedStyle(document.documentElement);
+  const colorScheme = rootStyles.getPropertyValue('color-scheme').trim();
+  if (colorScheme === 'dark') {
+      document.documentElement.style.colorScheme = 'light';
+      document.documentElement.removeAttribute('data-theme', 'dark');
+      images.forEach((img, index) => {
+        img.src = `../static/images/${imglst[index]}light.png`
+      });
+  
+  } else {
+      document.documentElement.style.colorScheme = 'dark';
+      document.documentElement.setAttribute('data-theme', 'dark');
+      images.forEach((img, index) => {
+        img.src = `../static/images/${imglst[index]}dark.png`
+      });
+  }
+}
+
+//Event Listener to set option Background on Click Event
+const optionLinks = document.querySelectorAll('.link');
+optionLinks.forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault(); 
+        options.value = this.getAttribute('data-option');
+        addingClassToOptions() 
+    });
+});
+
+//Event listener to set option background when option changes
+options.addEventListener('change',addingClassToOptions)
+function addingClassToOptions() {
+  optionLinks.forEach(member => {
+    if (member.getAttribute('data-option') === options.value) {
+      member.classList.add('link-active');
+    } else {
+      member.classList.remove('link-active');
+    }
+  });
+}
+
+//Event Listener to toggle the slider
+document.querySelectorAll('.right-slider .link').forEach(el => {
+  el.addEventListener('click', (event) => {
+    toggleSlider(event.target, 'none');
+  });
+});
+
+//This Function is used to toggle the slider
+function toggleSlider(direction) {
+  const leftSlider = document.getElementById('leftSlider');
+  const rightSlider = document.getElementById('rightSlider');
+  
+  if (direction === 'left') {
+    leftSlider.style.width = leftSlider.style.width === '100%' ? '0' : '100%';
+    rightSlider.style.width = '0';
+  } else if (direction === 'right') {
+    rightSlider.style.width = rightSlider.style.width === '100%' ? '0' : '100%';
+    leftSlider.style.width = '0';
+  }
+  else{
+    rightSlider.style.width = '0';
+    leftSlider.style.width = '0';
+  }
+}
+
+//This sets footer anchor click to scroll top 50px below insted of top 0
+document.querySelectorAll('.footer-button a').forEach(link => {
+  link.addEventListener('click', function (e) {
+      e.preventDefault();
+      
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+      
+      if (targetElement) {
+          window.scrollTo({
+              top: targetElement.offsetTop - 50, 
+              behavior: 'smooth'
+          });
+      }
+  });
+});
+
+//Slider of the Explaination Page
+let currentSlide = 0; 
+const totalSlides = document.querySelectorAll('.slide').length;
+const slidesWrapper = document.querySelector('.slides-wrapper');
+
+function goToNextSlide() {
+  currentSlide = (currentSlide + 1) % totalSlides;
+  slidesWrapper.style.transform = `translateX(-${currentSlide * 100}%)`; 
+}
+
+setInterval(goToNextSlide, 8000);
